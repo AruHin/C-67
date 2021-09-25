3aa8987d8244f84f32f1f11d926aae3696f0296b
AruHin <gautam.mbe@gmail.com>
2021-09-25 11:16:51 +0530

    created new files

---
+++ Story.js
@@ -0,0 +1,8 @@
+/*
+Image result for crocodile and the monkey short story
+One day, a crocodile came out of the river.
+ He swam up to the tree and told the monkey that he had travelled a long distance 
+ and was in search of food as he was very hungry. 
+ The kind monkey offered him a few rose apples.
+ The crocodile thanked him and asked him if he could visit the monkey again.
+*/
